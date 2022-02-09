c3e0e065c29a528bb14416869c4614afe341a02b
bjozsef02 <94976028+bjozsef02@users.noreply.github.com>
2022-02-09 15:18:15 +0100

    Create database.sql

---
+++ assets/database.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2022. Feb 09. 15:16
+-- Kiszolgáló verziója: 10.4.21-MariaDB
+-- PHP verzió: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `note_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `category_of_the_note`
+--
+
+CREATE TABLE `category_of_the_note` (
+  `note_id` int(11) NOT NULL,
+  `note_category` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `notes`
+--
+
+CREATE TABLE `notes` (
+  `note_id` int(11) NOT NULL,
+  `note_name` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `note_content` text COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `creation_date` date NOT NULL,
+  `last_saved` date NOT NULL,
+  `shared` tinyint(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `password` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `mail_address` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `shares`
+--
+
+CREATE TABLE `shares` (
+  `note_id` int(11) NOT NULL,
+  `mail_address` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `category_of_the_note`
+--
+ALTER TABLE `category_of_the_note`
+  ADD PRIMARY KEY (`note_id`);
+
+--
+-- A tábla indexei `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`note_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- A tábla indexei `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`mail_address`);
+
+--
+-- A tábla indexei `shares`
+--
+ALTER TABLE `shares`
+  ADD PRIMARY KEY (`note_id`),
+  ADD UNIQUE KEY `note_id` (`mail_address`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `category_of_the_note`
+--
+ALTER TABLE `category_of_the_note`
+  MODIFY `note_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `note_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `shares`
+--
+ALTER TABLE `shares`
+  MODIFY `note_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `category_of_the_note`
+--
+ALTER TABLE `category_of_the_note`
+  ADD CONSTRAINT `category_of_the_note_ibfk_1` FOREIGN KEY (`note_id`) REFERENCES `notes` (`note_id`);
+
+--
+-- Megkötések a táblához `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `registration` (`id`);
+
+--
+-- Megkötések a táblához `shares`
+--
+ALTER TABLE `shares`
+  ADD CONSTRAINT `shares_ibfk_1` FOREIGN KEY (`mail_address`) REFERENCES `registration` (`mail_address`),
+  ADD CONSTRAINT `shares_ibfk_2` FOREIGN KEY (`note_id`) REFERENCES `notes` (`note_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
